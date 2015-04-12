f9ff48f0fe0eca8b76d814991e49b57d26ab56dc
snazaran <snazaran@gmail.com>
2015-04-12 14:56:08 -0500

    complete address to coords

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -4,8 +4,8 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "What is the address you would like to find the latitude and longitude of?"
 
@@ -17,18 +17,20 @@ url_safe_street_address = URI.encode(street_address)
 
 # Your code goes below. Hints:
 
-# url_of_data_we_want = "???"
-# raw_data = ???
-# parsed_data = ???
+
+url_of_data_we_want = "https://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+
+raw_data = open(url_of_data_we_want).read
+parsed_data = JSON.parse(raw_data)
 
 # ...
 
 # Let's store the latitude in a variable called 'the_latitude',
 #   and the longitude in a variable called 'the_longitude'.
 
-# the_latitude = ???
-# the_longitude = ???
+the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
+ puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
